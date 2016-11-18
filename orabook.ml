77591b6142129ee5af9a5b318e42a8a1ba9e3486
chetnashah <chetshah80@gmail.com>
2016-11-18 13:50:20 +0530

    Adding useful toString functions for custom types tip

--- orabook.ml
+++ orabook.ml
@@ -4,6 +4,15 @@ let make_pair a b = (a,b);;
 
 let compose f g x = f (g x);;
 
-
+let rec repeat f n x = match n with
+    0 -> x
+  | _ -> repeat f (n-1) (f x);;
+  
+(* point free repeate/iterate in terms of repeated composition *)
+let rec iterate n f =
+  if n = 0 then (function x -> x)
+  else compose f (iterate (n-1) f);;
+  
+  
 let () = print_string "orabook.ml loaded";;
   
