e44bdb845d0a34e4292e7b3e91af0c0c3869a20b
chetnashah <chetshah80@gmail.com>
2017-01-29 12:31:44 +0530

    add exercise on streams

--- hickeych3exercises.ml
+++ hickeych3exercises.ml
@@ -19,3 +19,43 @@ let emp = fnEmpty;;
 let dx1 = fnAdd emp "x" 99;;
 let dx2 = fnAdd dx1 "y" 100;;
 let dx3 = fnAdd dx2 "z" 101;;
+
+
+(* Stream is an infinite sequence
+   that support hd(s) which returns an
+   item and tl(s) which returns a new
+   stream with the first element removed
+*)
+
+type stream = int -> int;;
+type addConstant = stream -> int -> stream;;
+type diffStreams = stream -> stream -> stream;;
+type stMap = (int -> int) -> stream -> stream;;
+
+let square y = y * y;;
+let s x = x;;
+let hd (s: stream) = s 0;;
+let tl (s: stream) = (fun i -> s (i + 1));;
+
+let streamOfIntegers = s;;
+let zeroth = hd streamOfIntegers;;
+let next = tl streamOfIntegers;;
+let first = hd next;;
+let nextnext = tl next;;
+
+(* try out hd (tl (tl (tl (tl (tl s)))))) *)
+
+let (+:) :addConstant = fun s c ->
+  (fun x -> x + c);;
+
+let (-|) :diffStreams = fun s1 s2 ->
+  (fun x -> (hd s1) - (hd s2));;
+
+let stMap: stMap = fun f s1 ->
+  fun x -> f x;;
+
+(* make a square stream with :
+   let sqs = stMap square streamOfIntegers
+*)
+
+
